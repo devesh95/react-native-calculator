1ecfa59d547a409d98de9b253765dfc1f0621c7b
Devesh Dayal <devesh95@gmail.com>
2017-11-15 15:21:34 -0500

    init

---
+++ App.js
@@ -0,0 +1,80 @@
+import React, {Component} from 'react';
+import { Text, View } from 'react-native';
+import Style from './src/Style';
+
+import InputButton from './src/InputButton';
+
+// Define the input buttons that will be displayed in the calculator.
+const inputButtons = [
+    [1, 2, 3, '/'],
+    [4, 5, 6, '*'],
+    [7, 8, 9, '-'],
+    [0, '.', '=', '+']
+];
+
+export default class Calculator extends Component {
+
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      inputValue: 0
+    }
+  }
+
+  render() {
+    return (
+      <View style={Style.rootContainer}>
+        <View style={Style.displayContainer}>
+          <Text style={Style.displayText}>
+            {this.state.inputValue}
+          </Text>
+        </View>
+        <View style={Style.inputContainer}>
+          {this._renderInputButtons()}
+        </View>
+      </View>
+    )
+  }
+
+  _renderInputButtons() {
+    let views = [];
+
+    for (var r = 0; r < inputButtons.length; r ++) {
+      let row = inputButtons[r];
+
+      let inputRow = [];
+      for (var i = 0; i < row.length; i ++) {
+        let input = row[i];
+
+        inputRow.push(
+          <InputButton 
+            value={input} 
+            onPress={this._onInputButtonPressed.bind(this, input)}
+            key={r + "-" + i} 
+          />
+        );
+      }
+
+      views.push(<View style={Style.inputRow} key={"row-" + r}>{inputRow}</View>)
+    }
+
+    return views;
+  }
+
+  _onInputButtonPressed(input) {
+    switch (typeof input) {
+      case 'number':
+        return this._handleNumberInput(input)
+    }
+  }
+
+  _handleNumberInput(num) {
+    let inputValue = (this.state.inputValue * 10) + num;
+
+    this.setState({
+        inputValue: inputValue
+    });
+  }
+}
+
